3eb50d6bc101ea5a029a99d5ddc7669519fa284e
Emil Rosendahl <emil.lunnergard@gmail.com>
2015-02-19 12:09:11 +0100

    fix

--- src/Front/Controller.php
+++ src/Front/Controller.php
@@ -2,9 +2,16 @@
 
 use GoBrave\Fogg\System\Controller as System;
 use GoBrave\Fogg\System\Response;
+use GoBrave\Fogg\System\Renderer;
 
 class Controller extends System
 {
+  protected $renderer;
+
+  public function __construct(Renderer $renderer) {
+    $this->renderer = $renderer;
+  }
+
   protected function response($vars = [], $type = Response::HTML) {
     return new Response($vars, $type);
   }
