efa821a21fd5eb79c5b91e3afe7f99a736e2c23a
Emil Rosendahl <emil.lunnergard@gmail.com>
2015-03-09 11:39:25 +0100

    Adds renderer

--- src/Admin/Controller.php
+++ src/Admin/Controller.php
@@ -2,13 +2,16 @@
 
 use GoBrave\Fogg\System\Controller as System;
 use GoBrave\Fogg\System\Response;
+use GoBrave\Fogg\System\Renderer;
 
 class Controller extends System
 {
   protected $session;
+  protected $renderer;
 
-  public function __construct(Session $session) {
-    $this->session = $session;
+  public function __construct(Session $session, Renderer $renderer) {
+    $this->session  = $session;
+    $this->renderer = $renderer;
   }
 
   protected function response($vars = [], $type = Response::HTML) {
