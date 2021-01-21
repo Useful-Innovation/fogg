e47163dbafe3224e4944cc86afa5ad1d4b1f4c9b
Peter Nilsson <peter@Peters-MacBook-Pro.local>
2021-01-21 13:00:54 +0100

    feat: imageIdToPath

--- src/System/Image.php
+++ src/System/Image.php
@@ -2,22 +2,43 @@
 
 trait Image
 {
-  protected function imageIdToUrl($image_id, $size = 'thumbnail') {
-    if(!$image_id) {
-      return '';
+    protected function imageIdToUrl($image_id, $size = 'thumbnail')
+    {
+        if (!$image_id) {
+            return '';
+        }
+
+        Util::setBlogToBase();
+        $src = wp_get_attachment_image_src($image_id, $size);
+        if (!$src) {
+            Util::restoreBlogToCurrent();
+            return false;
+        }
+
+        $url = ($src ? $src[0] : false);
+        $url = preg_replace('/\/sites\/\d/', '', $url);
+
+        Util::restoreBlogToCurrent();
+        return $url;
     }
 
-    Util::setBlogToBase();
-    $src = wp_get_attachment_image_src($image_id, $size);
-    if(!$src) {
-      Util::restoreBlogToCurrent();
-      return false;
+    protected function imageIdToPath($image_id, $size = 'thumbnail')
+    {
+        if (!$image_id) {
+            return '';
+        }
+
+        Util::setBlogToBase();
+        $src = get_attached_file($image_id);
+
+        if (!$src) {
+            Util::restoreBlogToCurrent();
+            return false;
+        }
+
+        $url = ($src ? $src : false);
+
+        Util::restoreBlogToCurrent();
+        return $url;
     }
-
-    $url = ($src ? $src[0] : false);
-    $url = preg_replace('/\/sites\/\d/', '', $url);
-
-    Util::restoreBlogToCurrent();
-    return $url;
-  }
 }
